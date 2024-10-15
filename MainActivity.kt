261191072d1026e0200c005e85427cb2db1968ff
Priyadarshini Mohapatra <119788697+priyaaaahere@users.noreply.github.com>
2024-10-15 13:14:49 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,9 +1,6 @@
-package com.example.login
+package com.example.pizza_app
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,15 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        var a = findViewById<EditText>(R.id.editTextText)
-        var b = findViewById<EditText>(R.id.editTextText2)
-        var buttn = findViewById<Button>(R.id.button)
-        var txtview = findViewById<TextView>(R.id.textView)
-        buttn.setOnClickListener {
-            var x = a.text.toString()
-            var y = b.text.toString()
-            txtview.text = "Your name is " + x + " and your phone number is " + y
-        }
+        setContentView(R.layout.activity_pizzaaa)
     }
 }
